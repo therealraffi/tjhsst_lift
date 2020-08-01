6e3ae90a020e5bf66a1855b041bd7ae518320e44
Raffu Khondaker <2022rkhondak@tjhsst.edu>
2020-08-01 01:50:12 -0400

    nav fix

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(".navbar-toggler-icon").click(function () {
+        $(".navbar").addClass("navbar-solid")
+    })
+    $(window).scroll(function () {
         var pos = $(document).scrollTop();
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        if(pos>=5){ // change logo color on hover
+        if (pos >= 1) { // change logo color on hover
             $(".navbar").addClass("navbar-solid")
 
-        }
-        else{
+        } else if(($( window ).width() < 768 && $(".navbar-collapse").is(":visible") == false) || ($( window ).width() > 768 && $(".navbar-collapse").is(":visible"))){
+
             $(".navbar").removeClass("navbar-solid")
 
         }
 
-       ////////////////////////////////////////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////////
-        
+        ////////////////////////////////////////////////////////////////////////////////////////
+
     }).scroll();
 })
